7ab76492d3b9963f09bcdb7444346c9dcf67ac4d
SoggySupernova <person121314151617@gmail.com>
2025-10-30 19:07:04 -0400

    Update makescripthelper.bat

--- scripts/batch/makescripthelper.bat
+++ scripts/batch/makescripthelper.bat
@@ -3,6 +3,7 @@ python scripts/text/removebold.py
 python scripts/text/extract_code_block.py
 python scripts/text/create_brackets.py
 python scripts/text/generate_plain_subtitles.py
-python scripts/text/clean_text.py subtitles.txt finalsub.txt
+python scripts/text/clean_text.py temp/subtitles.txt temp/finalsub.txt
 taskkill /IM ollama.exe /F
+
 exit
